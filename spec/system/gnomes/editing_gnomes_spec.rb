2daf347a46d04c1db4270bb93348913349f3fc3a
Camille Anelli <camille@keycoopt.com>
2019-10-13 09:34:06 +0200

    test editing gnomes

--- spec/system/gnomes/editing_gnomes_spec.rb
+++ spec/system/gnomes/editing_gnomes_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe "editing gnomes", type: :system do
   it "can edit gnomes" do
     visit "/gnomes"
 
-    find(:css, '#edit_'+Gnome.first.id.to_s).click
-    expect(page).to have_content("Update")
+    find(:css, '#edit_'+ Gnome.first.id.to_s).click
+    fill_in "Country", with: "Reunion"
+    click_button "Confirm update"
+    click_link "#{Gnome.first.name}"
 
-
-
+    expect(page).to have_content("Country: Reunion")
   end
 end
